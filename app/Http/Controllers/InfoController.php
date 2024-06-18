e89a853cac871372a8800a3b8a67c5b9835adea8
AntaresMugisho <antaresmugisho@gmail.com>
2024-06-18 19:40:36 +0200

    Resource route not working: moving to single routes

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -17,14 +17,6 @@ class InfoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -39,14 +31,6 @@ class InfoController extends Controller
     public function show(Info $info)
     {
         return response()->json($info);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Info $info)
-    {
-        //
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\InfoController;
+use App\Http\Controllers\InfoController;
 use App\Http\Controllers\Api\usersController;
 
 // use App\Http\Controllers\Api\CommunicareController as com;
@@ -12,8 +12,13 @@ use App\Http\Controllers\Api\usersController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+
 Route::get('/test',[usersController::class,'test']);
 Route::post('/login',[usersController::class,'loginAccess'])->name('Api.login');
 Route::post('/loginAs',[usersController::class,'loginAs'])->name('Api.loginAs');
 
-Route::resource("/info", [InfoController::class]);
+Route::get("/info", [InfoController::class, "index"]);
+Route::get("/info/show/{id}", [InfoController::class, "show"]);
+Route::get("/info/create/{id}", [InfoController::class, "store"]);
+Route::get("/info/edit/{id}", [InfoController::class, "update"]);
+Route::get("/info/delete/{id}", [InfoController::class, "destroy"]);
